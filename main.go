b1b56fc206ec4fbd5279e5a79cae1712143e5f71
jiuyuedongyang <lin909958300@yeah.net>
2021-05-31 07:50:44 +0800

    加入安全检查2

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"GitHub_cicd_test/pets"
 	"fmt"
 )
-var (
-	password="123456"
-)
+
 func main() {
+	var (
+		password="123456"
+	)
 	fmt.Printf("my password %s",password)
 	saying := pets.Cat()
 	fmt.Println(saying)
